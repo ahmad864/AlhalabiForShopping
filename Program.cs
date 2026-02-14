3d60e417073e54e36bf9ca64a73991e1113c0a03
RaghadMjbel1 <ahmadgg112233445@gmail.com>
2026-02-15 01:54:02 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,10 +2,15 @@ using Microsoft.EntityFrameworkCore;
 using AlhalabiShopping.Data;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// إضافة خدمات MVC
+builder.Services.AddControllersWithViews();
+
+// ربط DbContext مع SQLite
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// إعدادات الجلسة (Session)
 builder.Services.AddDistributedMemoryCache();
 builder.Services.AddSession(options => {
     options.IdleTimeout = TimeSpan.FromMinutes(30);
@@ -13,6 +18,8 @@ builder.Services.AddSession(options => {
 });
 
 var app = builder.Build();
+
+// استخدام الملفات الثابتة وRouting
 app.UseStaticFiles();
 app.UseRouting();
 app.UseSession();
@@ -21,6 +28,7 @@ app.UseAuthorization();
 // يبدأ التشغيل من صفحة Welcome
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Account}/{action=Welcome}/{id?}");
+    pattern: "{controller=Account}/{action=Welcome}/{id?}"
+);
 
 app.Run();
